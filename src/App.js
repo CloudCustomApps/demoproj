262ad713df2b2aeafd0331e664f25116b8305eac
amendon <ashwin.mendon@ausnetservices.com.au>
2021-04-27 13:14:00 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Album from "designops/src/Components/Album"
+
+function App() {
+  return (
+    <div >
+      <Album Heading="Photo Album" SubContent="Something short and leading about the collection below—its contents, the creator, etc.
+              Make it short and sweet, but not too short so folks don&apos;t simply skip over it
+              entirely." />
+    </div>
+  );
+}
+
+export default App;
