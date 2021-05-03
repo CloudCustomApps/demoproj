6eee51176de37b71d11cb00eb520bbc65cf4e1c4
somijaiswal23 <somi.jaiswal@iiitb.net>
2021-05-04 09:52:06 +1000

    Fixed issues with demoproj

--- src/App.js
+++ src/App.js
@@ -1,12 +1,92 @@
+import React from 'react';
 import './App.css';
-import Album from "designops/src/Components/Album"
+import {CustomCard, StickyFooter, SignIn, CustomModal} from "../node_modules/designops/dist/index.js";
+import {IMAGE} from './Constants';
+import { useState } from 'react';
+import {TextField} from '@material-ui/core';
 
 function App() {
+  const [openModal, setOpenModal] = useState(false);
+  const [modalHeader, setModalHeader] = useState('Heading');
+  const [subHeader, setSubHeader] = useState('This is a media card. You can use this section to describe the content.');
+  const [hideCancel, sethideCancel] = useState(true);
+  const [isEdit, setIsEdit] = useState(false);
+  const handleSuccess=()=>setOpenModal(false);
+  const handleClose=()=>setOpenModal(false);
+  const viewAction = (header, subHeader) => {
+    setModalHeader(header);
+    setSubHeader(subHeader);
+    sethideCancel(true);
+    setOpenModal(true);
+    setIsEdit(false);
+  };
+
+  const editAction = () => {
+    setOpenModal(true);
+    setIsEdit(true);
+    sethideCancel(false);
+  }
+  const headingHandler=(e)=>{
+    const {value} = e.target
+    setModalHeader(value)
+  }
   return (
-    <div >
-      <Album Heading="Photo Album" SubContent="Something short and leading about the collection below—its contents, the creator, etc.
-              Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-              entirely." />
+    <div>
+      {/* component 1 */}
+      <div class='container'>
+      <CustomCard 
+        viewAction={viewAction} 
+        editAction={editAction}
+        cardHeading='Heading 1'
+        cardSubHeading='I am a Card 1'
+        img={IMAGE}
+        cardClasses='cardClass'/>
+      <CustomCard 
+        viewAction={viewAction} 
+        cardHeading='Heading 2'
+        cardSubHeading='I am a Card 2'
+        img={IMAGE}
+        cardClasses='cardClass'/>
+        <CustomCard 
+        viewAction={viewAction} 
+        cardHeading='Heading 1'
+        cardSubHeading='I am a Card 1'
+        img={IMAGE}
+        cardClasses='cardClass'/>
+        <CustomCard 
+        viewAction={viewAction} 
+        cardHeading='Heading 1'
+        cardSubHeading='I am a Card 1'
+        img={IMAGE}
+        cardClasses='cardClass'/>
+        <CustomCard 
+        viewAction={viewAction} 
+        cardHeading='Heading 1'
+        cardSubHeading='I am a Card 1'
+        img={IMAGE}
+        cardClasses='cardClass'/>
+      </div>
+      {/* component 2*/}
+       <CustomModal 
+        isOpen={openModal} 
+        size="sm" 
+        handleSuccess={handleSuccess} 
+        handleClose={handleClose}
+        hideLetfBtn={hideCancel}
+        title={modalHeader}>
+        <p>{subHeader}</p>
+        {isEdit && <form>
+          <TextField id="outlined-basic" label="Heading" variant="outlined" type="text" fullWidth onChange={headingHandler}/>
+          <TextField id="outlined-basic" label="SubHeading" variant="outlined" type="text" fullWidth/>
+        </form>}
+      </CustomModal>
+
+      {/* component 3*/}
+      <SignIn/>
+
+      {/* component 4 */}
+      <StickyFooter/>
+
     </div>
   );
 }
---
+++ src/Constants.js
@@ -0,0 +1,34 @@
+export const CARDARRAY =[
+    {
+        id:1,
+        heading: 'Heading 1',
+        subheading:'I am Card 1'
+    },
+    {
+        id:2,
+        heading: 'Heading 2',
+        subheading:'I am Card 2'
+    },
+    {
+        id:3,
+        heading: 'Heading 3',
+        subheading:'I am Card 3'
+    },
+    {
+        id:4,
+        heading: 'Heading 4',
+        subheading:'I am Card 4'
+    },
+    {
+        id:5,
+        heading: 'Heading 5',
+        subheading:'I am Card 5'
+    },
+    {
+        id:6,
+        heading: 'Heading 6',
+        subheading:'I am Card 6'
+    }      
+];
+
+export const IMAGE = "https://images.unsplash.com/photo-1617665146086-14d150bd34fc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1000&amp;q=80";
