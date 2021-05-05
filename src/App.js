9c71f9724394f0d5eae66bc0dd54e3b02777f0bb
somijaiswal23 <somi.jaiswal@iiitb.net>
2021-05-05 11:19:59 +1000

    Using Color classes from designops

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import {CustomCard, StickyFooter, SignIn, CustomModal} from "../node_modules/designops/dist/index.js";
+import "../node_modules/designops/dist/designops.css";
 import {IMAGE} from './Constants';
 import { useState } from 'react';
 import {TextField} from '@material-ui/core';
@@ -47,36 +48,47 @@ function App() {
   }
 
   return (
-    <div>
-      {/* component 1 */}
-      <div className='container'>    
-        {cardRows(1,6)}
-      </div>   
-      {/* component 2*/}
-       <CustomModal 
-        isOpen={openModal} 
-        size="sm" 
-        handleSuccess={handleSuccess} 
-        handleClose={handleClose}
-        hideLetfBtn={hideCancel}
-        title={modalHeader}>
-        <p>{subHeader}</p>
-        {isEdit && <form>
-          <TextField id="outlined-basic" label="Heading" variant="outlined" type="text" fullWidth onChange={headingHandler}/>
-          <TextField id="outlined-basic" label="SubHeading" variant="outlined" type="text" fullWidth/>
-        </form>}
-      </CustomModal>
+    <div className="bg-gray-200">
+      <div className="wrapper">
 
-      {/* component 3*/}
-      <SignIn/>
+      {/* component 1 */}
+        <h1 className="text-primary">I am h1 component</h1>
+        <h2 className="text-red-400">I am h2 component</h2>
+        <h3 className="text-blue-500">I am h3 component</h3>
+        <h4 className="text-green-600"> I am h4 component</h4>
+        <h5 className="text-indigo-700">I am h5 component</h5>
+        <h6 className="text-yellow-900">I am h6 component</h6>
+      
+      {/* component 2 */}
+        <div className='container'>    
+          {cardRows(1,6)}
+        </div>
+
+      {/* component 3 */}
+        <CustomModal
+          isOpen={openModal} 
+          size="sm" 
+          handleSuccess={handleSuccess} 
+          handleClose={handleClose}
+          hideLetfBtn={hideCancel}
+          title={modalHeader}>
+          <p>{subHeader}</p>
+          {isEdit && <form>
+            <TextField id="outlined-basic" label="Heading" variant="outlined" type="text" fullWidth onChange={headingHandler}/>
+            <TextField id="outlined-basic" label="SubHeading" variant="outlined" type="text" fullWidth/>
+          </form>}
+        </CustomModal>
 
       {/* component 4 */}
-      <div className="bottom">
-        <StickyFooter copyright={`@${new Date().getFullYear()} RXP Services. All rights reserved`}
-          footerText='Creative agency making happier humans.' position={'left'}/>
-      </div>
-      
+        <SignIn/>
 
+        <div className="seperator"></div>
+      {/* component 5 */}
+        <div className="bottom bg-primary">
+          <StickyFooter copyright={`@${new Date().getFullYear()} RXP Services. All rights reserved`}
+            footerText='Creative agency making happier humans.' position={'left'}/>
+        </div>
+      </div>
     </div>
   );
 }
