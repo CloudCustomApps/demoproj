63c047258e2261f09317e7491cc505d151df97c9
rahulchauhan007 <rahul.chauhan@rxpservices.com>
2021-06-02 16:10:03 +1000

    SignIn and routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './App.css';
 import { StickyFooter, SignIn } from '../node_modules/designops/dist/index.js';
 import '../node_modules/designops/dist/designops.css';
 
 function App() {
+  let history = useHistory();
+  function handleClick() {
+    history.push('/dashboard');
+  }
   return (
     <div className="wrapper">
-      <SignIn />
-
+      <SignIn firstLabel="Please enter your email" handleClick={handleClick} />
       <StickyFooter
         copyright={`@${new Date().getFullYear()} RXP Services. All rights reserved`}
         footerText="Creative agency making happier humans."
