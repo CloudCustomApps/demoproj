7979039be74f4c06d093796e4c50defe52025a13
Jin <ming-jin.yeh@rxpservices.com>
2021-06-03 17:36:33 +1000

    feature: Center the signIn form

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { SignIn } from 'designops';
 import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
 const SignInPage = (props) => {
-  let history = useHistory();
+  const history = useHistory();
+  const classes = useStyles();
 
   function handleClick() {
     history.push('/dashboard');
   }
 
   return (
-    <SignIn firstLabel="Please enter your email" handleClick={handleClick} />
+    <div className={classes.container}>
+      <SignIn firstLabel="Please enter your email" handleClick={handleClick} />
+    </div>
   );
 };
 
 export default SignInPage;
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}));
