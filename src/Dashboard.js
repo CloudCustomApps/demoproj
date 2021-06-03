8f428974e381f0fd7d697c095d0e95913be72ba1
Jin <ming-jin.yeh@rxpservices.com>
2021-06-03 11:03:45 +1000

    feature: Add dashboard page

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Container } from '@material-ui/core';
+import { COUPONS } from './Constants';
+import CouponList from './components/CouponList';
 
 function Dashboard() {
-  return <div className="wrapper">develop new grid component</div>;
+  return (
+    <div className="wrapper">
+      <Container maxWidth="md">
+        <CouponList coupons={COUPONS} />
+      </Container>
+    </div>
+  );
 }
 
 export default Dashboard;
