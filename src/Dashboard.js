e1c3ee3200dba02bb76cbe0e261366639fb3923b
rahulchauhan007 <rahul.chauhan@rxpservices.com>
2021-06-03 21:59:28 +1000

    test

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,10 +3,12 @@ import './App.css';
 import { Container } from '@material-ui/core';
 import { COUPONS } from './Constants';
 import CouponList from './components/CouponList';
+import { SignIn, AppHeader } from 'designops';
 
 function Dashboard() {
   return (
-    <div className="wrapper">
+    <div className="flex">
+      <AppHeader />
       <Container maxWidth="md">
         <CouponList coupons={COUPONS} />
       </Container>
