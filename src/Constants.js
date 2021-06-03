e86d261c22629664470b402902f712f5e5913a02
Jin <ming-jin.yeh@rxpservices.com>
2021-06-03 16:56:41 +1000

    feature: Add createdAt property to mock data

--- src/Constants.js
+++ src/Constants.js
@@ -42,26 +42,31 @@ export const IMAGE =
 export const COUPONS = [
   {
     header:
+      'Logitech MX Keys Wireless Illuminated Keyboard $165.60 Delivered @ Amazon AU',
+    desc: 'Another day another MX deal, this time on Amazon. This is for anyone who missed out on the recent DigiDirect deal or thought they bought one only to realize DigiDirect routinely sell products on …',
+    image: 'https://files.ozbargain.com.au/n/67/627267.jpg?h=15a6d3cb',
+    createdAt: new Date(2021, 6, 2),
+  },
+  {
+    header:
       'NetGear Orbi RBK50 AC3000 Tri-Band Mesh Wi-Fi System 2 Pack $244 (Pickup Only) @ The Good Guys',
     desc: 'The Netgear Orbi High-Performance AC3000 Tri-Band WiFi System, is a high-end routing system with both a router and a satellite. The Orbi AC3000 has Tri-Band WiFi, with an extra spectrum for super',
     image: 'https://files.ozbargain.com.au/n/73/627273.jpg?h=e2102aa9',
+    createdAt: new Date(2021, 5, 20),
   },
   {
     header:
       'Quilton 3 Ply White Paper Towel (60 Sheets Per Roll), 12 Count, Pack of 12 $11.00/ $9.90 (Subscribe & Save) @ Amazon',
     desc: '3 Ply Thickness Strong when wet Ideal for cleaning kitchens, cooktops, bathrooms, windows and outdoor use at picnics Proudly made in Australia and Australian owned, providing jobs for over 600 of our',
     image: 'https://files.ozbargain.com.au/n/70/627270.jpg?h=eaa1a552',
+    createdAt: new Date(2021, 5, 13),
   },
-  {
-    header:
-      'Logitech MX Keys Wireless Illuminated Keyboard $165.60 Delivered @ Amazon AU',
-    desc: 'Another day another MX deal, this time on Amazon. This is for anyone who missed out on the recent DigiDirect deal or thought they bought one only to realize DigiDirect routinely sell products on …',
-    image: 'https://files.ozbargain.com.au/n/67/627267.jpg?h=15a6d3cb',
-  },
+
   {
     header:
       "$10 Bonus Cashback on $30 Spend at Any Online Store @ Cashrewards (Includes GC Page & PS Plus, Activation Req'd, Excludes eBay)",
     desc: 'Guys, confirmation that True Rewards has an issue with its widgets. Please be patient while this is hopefully fixed soon. Will keep you posted. Thank you. Hi everyone. Another ripper bonus for all …',
     image: 'https://files.ozbargain.com.au/n/60/627260.jpg?h=48838a34',
+    createdAt: new Date(2021, 4, 5),
   },
 ];
